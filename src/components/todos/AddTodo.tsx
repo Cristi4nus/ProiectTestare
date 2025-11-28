fe54052d3e198ad35ae8937391ab77b1a134695e
521_nor <nora_oprescu@yahoo.com>
2025-11-28 14:39:37 +0200

    adaugat traducere AddTodo

--- src/components/todos/AddTodo.tsx
+++ src/components/todos/AddTodo.tsx
@@ -2,14 +2,17 @@ import * as actions from "@/actions";
 import Button from "../button/Button";
 import Form from "../form/Form";
 import Input from "../input/Input";
+import { useTranslations } from "next-intl";
+
 
 const AddTodo = () => {
+  const t = useTranslations('AddTodo');
   return (
     <div>
       <Form action={actions.createTodo}>
         <div className="flex gap-4 items-center">
-          <Input name="input" type="text" placeholder="Add Todo Here..." />
-          <Button type="submit" text="Add" bgColor="bg-blue-600" />
+          <Input name="input" type="text" placeholder={t("placeholder")} />
+          <Button type="submit" text={t("button")} bgColor="bg-blue-600" />
         </div>
       </Form>
     </div>
