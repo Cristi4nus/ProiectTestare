292ef56fd6cad1e3c30f5ed58d0e42a51da30dd7
ANDREEA BĂRĂIAN <andreea.baraian@stud.ubbcluj.ro>
2026-01-06 20:06:11 +0200

    Input required for test case

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -8,6 +8,7 @@ const Input = ({ name, type, placeholder, value }: inputProps) => {
         type={type}
         placeholder={placeholder}
         value={value}
+        required
         className="block w-full p-5 mx-2 border rounded-xl text-lg bg-white/10 border-white/20 placeholder-gray-300 text-white backdrop-blur-sm focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent transition-all"
       />
     </div>
